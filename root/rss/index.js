9745266d6200edd1c7130590b323a53a3cc1db40
Mofei Zhu <13761509829@163.com>
2016-03-13 22:50:18 +0800

    create index.js

---
+++ root/rss/index.js
@@ -0,0 +1,22 @@
+var Mongo = require('../../system/mongo/init.js');
+
+var render = function() {
+    var self = this;
+
+    Mongo.open(function(db) {
+        var collection = db.collection('blog');
+        collection.find({}, { limit: 10, skip: 0, sort: { _id: -1 } }).toArray(function(err, docs) {
+            self.jade({
+                header: {
+                    'Content-Type': 'application/xml'
+                },
+                data: {
+                    blogs: docs
+                }
+            })
+        });
+
+    });
+}
+
+exports.get = render;
