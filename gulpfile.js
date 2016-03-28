6e5dfc5e292e0c39d4775624442d290ebc508292
zmofei <13761509829@163.com>
2016-03-28 22:03:52 +0800

    use gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ const pngquant = require('imagemin-pngquant');
 const rev = require('gulp-rev');
 const revReplace = require('gulp-rev-replace');
 const sass = require('gulp-sass');
+const watch = require('gulp-watch')
 
 gulp.task('imagemin', () => {
-  return gulp.src('_dev/static/image/**/*')
+  return gulp.src('build/static/image/**/*')
     .pipe(imagemin({
       progressive: true,
       svgoPlugins: [{
@@ -14,54 +15,61 @@ gulp.task('imagemin', () => {
       }],
       use: [pngquant()]
     }))
-    .pipe(gulp.dest('_dev/build/static/image'));
+    .pipe(gulp.dest('build/static/image/'));
 });
 
 gulp.task('rev', () => {
-  return gulp.src('_dev/static/**/*')
-    .pipe(gulp.dest('_dev/build/static/'))
+  return gulp.src('build/static/**/*')
+    .pipe(gulp.dest('build/static/'))
     .pipe(rev())
-    .pipe(gulp.dest('_dev/build/static/'))
+    .pipe(gulp.dest('build/static/'))
     .pipe(rev.manifest())
-    .pipe(gulp.dest('_dev/build/static/'));
+    .pipe(gulp.dest('build/static/'));
 });
 
 gulp.task("revreplaceJade", ['rev'], () => {
-  var manifest = gulp.src("_dev/build/static/rev-manifest.json");
-  return gulp.src('_dev/root/**/*.jade')
-    .pipe(gulp.dest('_dev/build/root/'))
+  var manifest = gulp.src("build/static/rev-manifest.json");
+  return gulp.src('build/root/**/*.jade')
+    .pipe(gulp.dest('build/root/'))
     .pipe(revReplace({
       manifest: manifest,
       replaceInExtensions: '.jade'
     }))
-    .pipe(gulp.dest('_dev/build/root/'));
+    .pipe(gulp.dest('build/root/'));
 });
 
 gulp.task("revreplaceStatic", ['rev'], () => {
-  var manifest = gulp.src("_dev/build/static/rev-manifest.json");
-  return gulp.src('_dev/build/static/**/*')
+  var manifest = gulp.src("build/static/rev-manifest.json");
+  return gulp.src('build/static/**/*')
     .pipe(revReplace({
       manifest: manifest
     }))
-    .pipe(gulp.dest('_dev/build/static/'));
+    .pipe(gulp.dest('build/static/'));
+});
+
+gulp.task('prepareStatic', ['sass_noWatch'], () => {
+  return gulp.src('static/**/*')
+    .pipe(gulp.dest('build/static/'))
+});
+
+gulp.task('prepare', ['prepareStatic'], () => {
+  return gulp.src('root/**/*')
+    .pipe(gulp.dest('build/root/'))
 });
 
 gulp.task('sass', () => {
-  return gulp.src('_dev/sass/**/*.scss')
+  return gulp.src('dev/scss/**/*.scss')
+    .pipe(watch('dev/scss/**/*.scss'))
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('_dev/static/css/'));
+    .pipe(gulp.dest('static/css/'));
+});
+
+gulp.task('sass_noWatch', () => {
+  return gulp.src('dev/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('static/css/'));
 })
 
-gulp.task('clean', () => {
-  return del(['_dev/build']);
-});
+gulp.task('default', ['sass']);
 
-gulp.task('copy', () => {
-  gulp.src('_dev/build/root/**/*')
-    .pipe(gulp.dest('root/'));
-  gulp.src('_dev/build/static/**/*')
-    .pipe(gulp.dest('static/'));
-});
-
-
-gulp.task('default', ['imagemin', 'sass', 'revreplaceJade', 'revreplaceStatic', 'copy'])
+gulp.task('online', ['prepare','imagemin','revreplaceJade', 'revreplaceStatic']);
