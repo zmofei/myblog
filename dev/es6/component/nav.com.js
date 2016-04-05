d009351c46eb1469a74d2304e2ef9ece74e0efde
zmofei <13761509829@163.com>
2016-04-05 14:56:15 +0800

    update & improve the avatar

--- dev/es6/component/nav.com.js
+++ dev/es6/component/nav.com.js
@@ -2,7 +2,6 @@ class Nav {
     constructor() {
         var navTar = document.getElementById('navTar');
         var navLink = document.querySelector('.global-nav-text-link');
-        console.log('??223?');
         navTar.addEventListener('click', function(e) {
             if (navLink.style.display == 'block') {
                 navLink.style.display = 'none';
