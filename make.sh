6b5494c84aea90c3e2304d874fae31b07b5f8449
zmofei <13761509829@163.com>
2016-04-13 16:09:19 +0800

    add make.sh

---
+++ make.sh
@@ -0,0 +1,22 @@
+type=$1;
+if [[ $type == 'online' ]]
+then
+    echo '**** online';
+    echo "- var host = '//o4qdeqynn.qnssl.com';" > system/config/config.jade;
+	echo '$host: "//o4qdeqynn.qnssl.com";' > system/config/config.scss;
+    echo '**** clean';
+    rm -rf ./build/
+    echo '**** run config';
+	gulp online;
+    echo '**** pack';
+    FileName=`date +%y_%m_%d_%H_%M_%S`;
+    tar -cvzf build_$FileName.tar.gz ./build;
+    scp -r ./build_$FileName.tar.gz root@zhuwenlong.com:/usr/local/www/zhuwenlong.com/myblog/receive/build_$FileName.tar.gz;
+else
+    echo '**** dev';
+    echo '**** setting config';
+    echo "- var host = '';" > ./system/config/config.jade;
+    echo '$host: "";' > ./system/config/config.scss;
+    echo '**** run gulp';
+    gulp;
+fi
