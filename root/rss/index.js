3068460e17101b36948da5343c7168ae196a57f0
z.mofei <13761509829@163.com>
2016-10-11 21:12:41 +0800

    fix rss utf-8

--- root/rss/index.js
+++ root/rss/index.js
@@ -1,11 +1,47 @@
 var Mongo = require('../../system/mongo/init.js');
 
-var render = function() {
+var encode = function (strUni) {
+    var strUtf = strUni.replace(/./g, function (c) {
+        var cc = c.charCodeAt(0);
+        if(cc<32){
+            return '';
+        }else{
+            return c;
+        }
+        // return String.fromCharCode(0xc0 | cc >> 6, 0x80 | cc & 0x3f);
+    });
+    // var strUtf = strUni.replace(/[\u0080-\u07ff]/g, function (c) {
+    //     var cc = c.charCodeAt(0);
+    //     return String.fromCharCode(0xc0 | cc >> 6, 0x80 | cc & 0x3f);
+    // });
+
+    // strUtf = strUtf.replace(/[\u0800-\uffff]/g, function (c) {
+    //     var cc = c.charCodeAt(0);
+    //     return String.fromCharCode(0xe0 | cc >> 12, 0x80 | cc >> 6 & 0x3F, 0x80 | cc & 0x3f);
+    // });
+
+    return strUtf;
+};
+
+var render = function () {
     var self = this;
 
-    Mongo.open(function(db) {
+    Mongo.open(function (db) {
         var collection = db.collection('blog');
-        collection.find({}, { limit: 10, skip: 0, sort: { _id: -1 } }).toArray(function(err, docs) {
+        collection.find({}, {
+            limit: 10,
+            skip: 0,
+            sort: {
+                _id: -1
+            }
+        }).toArray(function (err, docs) {
+            for (var i in docs) {
+                var item = docs[i];
+                item.html = encode(item.html);
+                // console.log()
+            };
+
+            // console.log('@@@',docs);
             self.jade.render({
                 header: {
                     'Content-Type': 'application/xml; charset=utf-8'
