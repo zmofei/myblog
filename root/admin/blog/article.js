592e86d37417d226bc1546e73edcbfe043548bb6
z.mofei <13761509829@163.com>
2016-10-12 23:19:28 +0800

    update

--- root/admin/blog/article.js
+++ root/admin/blog/article.js
@@ -1,5 +1,28 @@
-var render = function() {
-    this.jade.render();
+var Mongo = require('../../../system/mongo/init.js');
+
+var get = function () {
+    var self = this;
+
+    Mongo.open(function (db) {
+        var classCollection = db.collection('blog_class');
+        var data = {};
+        var getBlogClass = new Promise(function (resolve, reject) {
+            classCollection.find({}).toArray(function (err, docs) {
+                var blogClass = {};
+                for (var i in docs) {
+                    blogClass[docs[i].classid] = docs[i];
+                }
+                data.blogClass = blogClass;
+                resolve();
+            });
+        });
+
+        Promise.all([getBlogClass]).then(function (val) {
+            self.jade.render({
+                data: data
+            });
+        });
+    });
 }
 
-exports.get = render;
+exports.get = get;
