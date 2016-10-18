4af6f306793c811a67bf4a847ce835c183630e4c
z.mofei <13761509829@163.com>
2016-10-18 23:33:48 +0800

    update

---
+++ root/admin/blog/article_post.js
@@ -0,0 +1,70 @@
+var Mongo = require('../../../system/mongo/init.js');
+
+var qs = require('querystring');
+
+var post = function () {
+
+    var self = this;
+
+    var body = '';
+    this.req.on('data', function (data) {
+        body += data;
+        if (body.length > 1e6) {
+            this.req.connection.destroy();
+        }
+    });
+
+    this.req.on('end', function () {
+        var post = qs.parse(body);
+
+        Mongo.open(function (db) {
+            db.collection('blog').insertOne({
+                "classid": post.class ? post.class.join(',') : null,
+                "state": post.state,
+                "content": post.content,
+                "title": post.title,
+                "pubtime": new Date(),
+            }, function (err, res) {
+                if (err) {
+                    self.response.json({
+                        code: 400,
+                        data: post
+                    });
+                } else {
+                    self.response.json({
+                        code: 200,
+                        data: post
+                    });
+                }
+            })
+        })
+    })
+
+    // Mongo.open(function (db) {
+    //     // var classCollection = db.collection('blog_class');
+    //     // var data = {};
+    //     // var getBlogClass = new Promise(function (resolve, reject) {
+    //     //     classCollection.find({}).toArray(function (err, docs) {
+    //     //         var blogClass = {};
+    //     //         for (var i in docs) {
+    //     //             blogClass[docs[i].classid] = docs[i];
+    //     //         }
+    //     //         data.blogClass = blogClass;
+    //     //         resolve();
+    //     //     });
+    //     // });
+    //     // this.res.writeHead(200, {
+    //     //     'Content-Type': 'text/plain'
+    //     // });
+    //     self.response.json({
+    //         msg: 'got it'
+    //     });
+    //     // Promise.all([getBlogClass]).then(function (val) {
+    //     // self.jade.render({
+    //     // data: data
+    //     // });
+    //     // });
+    // });
+}
+
+exports.post = post;
--- root/blog/index.js
+++ root/blog/index.js
@@ -3,7 +3,7 @@ var Mongo = require('../../system/mongo/init.js');
 var url = require('url');
 
 
-var render = function() {
+var render = function () {
     var self = this;
     var page = 0;
     var perPage = 10;
@@ -14,31 +14,41 @@ var render = function() {
         skip = perPage * page;
     }
 
-    Mongo.open(function(db) {
+    Mongo.open(function (db) {
         var collection = db.collection('blog');
         var classCollection = db.collection('blog_class');
 
         var url_parts = url.parse(self.req.url, true);
         var search = url_parts.search;
 
-        var findquery = {};
+        var findquery = {
+            "state": {
+                $in: ["0", 0, null]
+            }
+        };
         if (url_parts.query.tags) {
             var newTags = [];
             var tags = url_parts.query.tags.split(',');
             for (var i in tags) {
                 newTags.push(tags[i]);
                 newTags.push(parseInt(tags[i]));
+                newTags.push(new RegExp(tags[i]));
+                
             }
-            findquery.classid = { $in: newTags }
+            findquery.classid = {
+                $in: newTags
+            }
         }
+
+        console.log(findquery);
 
         var data = {
             currentPage: page + 1,
             search: search,
         };
 
-        var getBlogClass = new Promise(function(resolve, reject) {
-            classCollection.find({}).toArray(function(err, docs) {
+        var getBlogClass = new Promise(function (resolve, reject) {
+            classCollection.find({}).toArray(function (err, docs) {
                 var blogClass = {};
                 for (var i in docs) {
                     blogClass[docs[i].classid] = docs[i];
@@ -48,36 +58,47 @@ var render = function() {
             });
         });
 
-        var getCount = new Promise(function(resolve, reject) {
-            collection.find(findquery).count(function(err, count) {
+        var getCount = new Promise(function (resolve, reject) {
+            collection.find(findquery).count(function (err, count) {
                 data.totalPage = Math.ceil(count / perPage);
                 resolve();
             });
         });
 
-        var getBlog = new Promise(function(resolve, reject) {
-            collection.find(findquery, { limit: perPage, skip: skip, sort: { _id: -1 } }).toArray(function(err, docs) {
+        var getBlog = new Promise(function (resolve, reject) {
+            collection.find(findquery, {
+                limit: perPage,
+                skip: skip,
+                sort: {
+                    _id: -1
+                }
+            }).toArray(function (err, docs) {
                 // console.log(docs);
                 data.blogs = docs;
                 resolve();
             });
         });
 
-        Promise.all([getBlogClass, getCount, getBlog]).then(function(val) {
+        Promise.all([getBlogClass, getCount, getBlog]).then(function (val) {
             for (var i in data.blogs) {
                 data.blogs[i].tags = [];
                 var _class = data.blogs[i].classid;
                 var tagIds = '';
-                if (typeof(_class) == 'string') {
+                if (typeof (_class) == 'string') {
                     tagIds = [_class]
                 } else {
                     tagIds = _class
                 }
+
                 for (var j in tagIds) {
-                    data.blogs[i].tags.push({
-                        id: tagIds[j],
-                        name: data.blogClass[tagIds[j]].classname
+                    var ids = tagIds[j].split(',');
+                    ids.map(function (item) {
+                        data.blogs[i].tags.push({
+                            id: item,
+                            name: data.blogClass[item] ? data.blogClass[item].classname : ''
+                        });
                     });
+
                 }
             }
             self.jade.render({
