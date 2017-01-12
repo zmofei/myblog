421a055c0cf4e9b83e60ff643d7b3e025f386359
Mofei Zhu <13761509829@163.com>
2017-01-12 19:31:11 +0800

    auto deploy

--- make.sh
+++ make.sh
@@ -12,6 +12,8 @@ then
     FileName=`date +%y_%m_%d_%H_%M_%S`;
     tar -cvzf build_$FileName.tar.gz ./build;
     scp -r ./build_$FileName.tar.gz root@zhuwenlong.com:/usr/local/zhuwenlong/www/zhuwenlong.com/receive/build_$FileName.tar.gz;
+    CP="cd /usr/local/zhuwenlong/www/zhuwenlong.com/receive/; rm -rf build; tar -xvzf build_$FileName.tar.gz; cd ..; cp -r receive/build/root/* ./root/; cp -r receive/build/static/* ./static/;"
+    ssh root@zhuwenlong.com "${CP}";
 else
     echo '**** dev';
     echo '**** setting config';
