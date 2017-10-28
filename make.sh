4fd2a635ecba13f42c680d68e8df70c46c08e956
zmofei <13761509829@163.com>
2017-10-28 13:08:25 +0800

    background video support safari

--- make.sh
+++ make.sh
@@ -11,8 +11,8 @@ then
     echo '**** pack';
     FileName=`date +%y_%m_%d_%H_%M_%S`;
     tar -cvzf build_$FileName.tar.gz ./build;
-    scp -r ./build_$FileName.tar.gz root@zhuwenlong.com:/usr/local/zhuwenlong/www/zhuwenlong.com/receive/build_$FileName.tar.gz;
-    CP="cd /usr/local/zhuwenlong/www/zhuwenlong.com/receive/; rm -rf build; tar -xvzf build_$FileName.tar.gz; cd ..; cp -r receive/build/root/* ./root/; cp -r receive/build/static/* ./static/;"
+    scp -r ./build_$FileName.tar.gz root@zhuwenlong.com:/usr/local/zhuwenlong/www/zhuwenlong.com/myblog/receive/build_$FileName.tar.gz;
+    CP="cd /usr/local/zhuwenlong/www/zhuwenlong.com/myblog/receive/; rm -rf build; tar -xvzf build_$FileName.tar.gz; cd ..; cp -r receive/build/root/* ./root/; cp -r receive/build/static/* ./static/;"
     ssh root@zhuwenlong.com "${CP}";
     rm ./build_$FileName.tar.gz;
 else
