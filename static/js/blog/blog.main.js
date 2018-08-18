eb0b515dbe9f49327869a146c171ed9698c02684
zmofei <13761509829@163.com>
2018-08-18 23:39:48 +0800

    feat(update ui):

--- static/js/blog/blog.main.js
+++ static/js/blog/blog.main.js
@@ -14,3 +14,15 @@ class Nav {
 }
 
 new Nav();
+
+window.onload = function() {
+    var searchBtn = document.getElementById('blog-search-tar');
+    var searchBox = document.getElementById('blog-search-box');
+    var searchCls = document.getElementById('blog-search-cls');
+    searchBtn.addEventListener('click', function() {
+        searchBox.className = 'blog-search blog-search-active';
+    });
+    searchCls.addEventListener('click', function() {
+        searchBox.className = 'blog-search';
+    });
+}
