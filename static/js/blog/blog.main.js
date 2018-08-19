8d590bb30222e677a210193261369b703813691c
zmofei <13761509829@163.com>
2018-08-19 23:24:50 +0800

    feat:

--- static/js/blog/blog.main.js
+++ static/js/blog/blog.main.js
@@ -14,3 +14,16 @@ class Nav {
 }
 
 new Nav();
+
+
+window.onload = function() {
+    var searchBtn = document.getElementById('blog-search-tar');
+    var searchBox = document.getElementById('blog-search-box');
+    var searchCls = document.getElementById('blog-search-cls');
+    searchBtn.addEventListener('click', function() {
+        searchBox.className = 'blog-search blog-search-active';
+    });
+    searchCls.addEventListener('click', function() {
+        searchBox.className = 'blog-search';
+    });
+}
