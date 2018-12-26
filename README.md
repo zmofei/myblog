1ff3b3515c564ed85e72eeec1b283b62d90579c6
zmofei <13761509829@163.com>
2018-12-26 16:07:48 +0800

    update

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 new version of my blog http://www.zhuwenlong.com
 
 use the new webserver for Node.js `Dufing` https://github.com/zmofei/dufing
+
+## Deploy
+
+```
+node app.js
+```
+
+## Dev
+
+```
+NODE_ENV=dev node app.js
+```
--- make.sh
+++ make.sh
@@ -2,7 +2,6 @@ type=$1;
 if [[ $type == 'online' ]]
 then
     echo '**** online';
-    echo "- var host = '//static.zhuwenlong.com';" > system/config/config.jade;
 	echo '$host: "//static.zhuwenlong.com";' > system/config/config.scss;
     echo '**** clean';
     rm -rf ./build/
@@ -15,11 +14,9 @@ then
     CP="cd /usr/local/zhuwenlong/www/myblog/receive/; rm -rf build; tar -xvzf build_$FileName.tar.gz; cd ..; cp -r receive/build/root/* ./root/; cp -r receive/build/static/* ./static/;"
     ssh root@zhuwenlong.com "${CP}";
     rm ./build_$FileName.tar.gz;
-    # /usr/local/zhuwenlong/www/myblog
 else
     echo '**** dev';
     echo '**** setting config';
-    echo "- var host = '';" > ./system/config/config.jade;
     echo '$host: "";' > ./system/config/config.scss;
     echo '**** run gulp';
     ./node_modules/.bin/gulp;
