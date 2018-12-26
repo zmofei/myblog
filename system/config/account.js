b3e7879b16f2379092284e8557f73a047aa47321
zmofei <13761509829@163.com>
2018-12-26 16:29:48 +0800

    fix pwd

---
+++ system/config/account.js
@@ -0,0 +1,15 @@
+exports.email = {
+  user: process.env.EMAIL,
+  pass: process.env.EMAILPWD
+};
+
+exports.db = {
+  user: process.env.DB,
+  pwd: process.env.DBPWD,
+  dbName: process.env.DBNAME
+}
+
+exports.weixin = {
+  appid: process.env.WXID,
+  appsecret: process.env.WXSECRET
+}
