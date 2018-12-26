3a6ce7c18f7fc19b559a0a413be9bf221a40e367
zmofei <13761509829@163.com>
2018-12-26 17:26:33 +0800

    update mongodb

--- system/mongo/init.js
+++ system/mongo/init.js
@@ -16,24 +16,32 @@ var open = (function() {
       cb && callbacks.push(cb);
       if (!connectionStatus) {
         // connect to the db
-        var url = 'mongodb://' + (process.env.DBADDRESS || '127.0.0.1') + ':27017/' + config.db.dbName;
-        MongoClient.connect(url, function(err, _db) {
-          err && console.log('mongodb connent error ', err);
-          _db.authenticate(config.db.user, config.db.pwd, function(err, result) {
-            err && console.log('mongodb authenticate error ', err);
-            // console.log("Connected correctly to server [system/mongo/init.js]");
-            var _cb = callbacks.shift();
-            while (_cb) {
-              if (typeof(_cb) == 'function') {
-                _cb(_db);
-              } else {
-                console.log(_cb + ' not a function pass [system/mongo/init.js]');
-              }
-              _cb = callbacks.shift();
+        const url = `mongodb://${process.env.DBADDRESS || '127.0.0.1'}:27017`;
+        const dbName = config.db.dbName;
+        console.log(`++++ connect to ${url}`)
+        MongoClient.connect(url, {
+          useNewUrlParser: true,
+          auth: {
+            user: config.db.user,
+            password: config.db.pwd
+          }
+        }, function(err, _db) {
+          //   err && console.log('mongodb connent error ', err);
+          //   _db.authenticate(config.db.user, config.db.pwd, function(err, result) {
+          err && console.log('mongodb authenticate error ', err);
+          // console.log("Connected correctly to server [system/mongo/init.js]");
+          var _cb = callbacks.shift();
+          while (_cb) {
+            if (typeof(_cb) == 'function') {
+              _cb(_db);
+            } else {
+              console.log(_cb + ' not a function pass [system/mongo/init.js]');
             }
-            db = _db;
-            connectionStatus = 2;
-          })
+            _cb = callbacks.shift();
+          }
+          db = _db.db(config.db.dbName);
+          connectionStatus = 2;
+          //   })
 
         });
         connectionStatus = 1;
